6f4d1ae2d18a1e368d3eedd9612e5d37096ba0d3
Matthew Haines <m.haines1000@gmail.com>
2019-03-30 16:18:41 -0400

    more

--- connection/client.py
+++ connection/client.py
@@ -5,14 +5,14 @@ import time
 async def query():
     async with websockets.connect(
         'ws://flightcontroller:8765') as websocket:
-        data = input()
-        cur_time = int(round(time.time() * 1000))
-        await websocket.send(data)
-        response = await websocket.recv()
-        time_diff = int(response) - cur_time
-        print(f"Lag: {time_diff} ms")
+        while True:
+            data = input()
+            cur_time = int(round(time.time() * 1000))
+            await websocket.send(data)
+            response = await websocket.recv()
+            time_diff = int(response) - cur_time
+            print(f"Lag: {time_diff} ms")
 
 
-while True:
-    asyncio.get_event_loop().run_until_complete(query())
+asyncio.get_event_loop().run_until_complete(query())
 
