e07545a1a2d55595ff2ab2641b54c0985f875d0e
Matthew Haines <m.haines1000@gmail.com>
2019-04-14 22:06:13 -0400

    server object

--- connection/client.py
+++ connection/client.py
@@ -4,14 +4,10 @@ import time
 
 async def query():
     async with websockets.connect(
-        'ws://flightcontroller:8765') as websocket:
-        while True:
-            data = input()
-            cur_time = int(round(time.time() * 1000))
-            await websocket.send(data)
-            response = await websocket.recv()
-            time_diff = int(response) - cur_time
-            print(f"Lag: {time_diff} ms")
+        'ws://localhost:8765') as websocket:
+            for i in range(50):
+                await websocket.send("ping")
+                time.sleep(0.05)
 
 
 asyncio.get_event_loop().run_until_complete(query())
--- connection/server.py
+++ connection/server.py
@@ -1,17 +1,63 @@
-from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+import asyncio
+import websockets
 import time
-
-class Server(WebSocket):
-
-    def handleMessage(self):
-        self.sendMessage("{}".format(round(time.time() * 1000)))
-
-    def handleConnected(self):
-        print('connected')
-
-    def handleClose(self):
-        print('closed')
+import json
+import threading
 
 
-server = SimpleWebSocketServer('', 8765, Server)
-server.serveforever()
+def get_current_time():
+    return int(round(time.time() * 1000))
+
+
+class Server:
+
+    def __init__(self, port: int = 8765, host: str = 'localhost', timeout_time: float = 100):
+        """
+        Complete Websocket server and response handler.
+
+        Parameters:
+        port: The port that the server listens on
+
+        host: the hostname of the server
+
+        timeout_time: Maximum amount of time in milliseconds that the server will wait after each message
+            for the next message before initiating a safety shutdown procedure for the quadcopter.
+
+
+        """
+        # General
+        self.timeout_time = timeout_time
+
+        # Start Websockets Server
+        self.last_command = None
+        # asyncio.run(self.__serve(port, host))
+        threading.Thread(target=self.__serve)
+        print('here')
+        # Actual controller (change)
+        self.controller = None
+
+    def __timeout_check(self):
+        while (1):
+            if get_current_time() - self.last_command > self.timeout_time:
+                print('timed out')
+                self.controller.abort_sequence()
+                self.timeout_check.join()
+
+    async def __handler(self, websocket: websockets.server.WebSocketServer, path: str):
+        while (1):
+            command = await websocket.recv()
+            if self.last_command == None:
+                self.last_command = get_current_time()
+                self.timeout_check = threading.Thread(target=self.__timeout_check)
+                self.timeout_check.start()
+
+            print(command)
+            self.last_command = get_current_time()
+            # parsed = json.loads(command)
+
+    async def __serve(self, port: int, host: str):
+        server = await websockets.serve(self.__handler, host, port)
+        await server.wait_closed()
+
+
+server = Server()
